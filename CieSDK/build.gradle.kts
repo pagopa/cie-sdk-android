748e770da79b2c3ff5d109e6f0709e8413b83925
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2025-05-05 14:46:32 +0200

    chore: bump library version to 0.1.2

--- CieSDK/build.gradle.kts
+++ CieSDK/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 mavenPublishing {
-    coordinates("it.pagopa.io.app.cie", "cie", "0.1.1")
+    coordinates("it.pagopa.io.app.cie", "cie", "0.1.2")
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
