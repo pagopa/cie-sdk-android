14d523370000bc230f46251c6301bfac38560013
Carlo <carlo.dechellis@digitouch.it>
2026-02-10 11:26:21 +0100

    removed FLAG_LISTEN_DISABLE

--- CieSDK/src/main/java/it/pagopa/io/app/cie/nfc/NfcImpl.kt
+++ CieSDK/src/main/java/it/pagopa/io/app/cie/nfc/NfcImpl.kt
@@ -11,7 +11,6 @@ import it.pagopa.io.app.cie.cie.OnTransmit
 import it.pagopa.io.app.cie.cie.ReadCie
 import it.pagopa.io.app.cie.cie.transmitLogic
 import it.pagopa.io.app.cie.findActivity
-import kotlin.or
 
 internal class NfcImpl private constructor() : BaseNfcImpl() {
     private var adapter: NfcAdapter? = null
@@ -36,8 +35,6 @@ internal class NfcImpl private constructor() : BaseNfcImpl() {
                 NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK
         if (!doSound)
             flags = flags or NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS
-        if (android.os.Build.VERSION.SDK_INT >= 35)
-            flags = flags or NfcAdapter.FLAG_LISTEN_DISABLE
         try {
             adapter?.enableReaderMode(
                 activity, {
