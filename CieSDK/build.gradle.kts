5aed46485c20e6ce3a430dfec908c26a4831ff67
Federico Mastrini <federicomastrini93@gmail.com>
2026-05-11 11:23:20 +0200

    chore: bump version to 0.1.11

--- CieSDK/build.gradle.kts
+++ CieSDK/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 mavenPublishing {
-    coordinates("it.pagopa.io.app.cie", "cie", "0.1.10")
+    coordinates("it.pagopa.io.app.cie", "cie", "0.1.11")
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
